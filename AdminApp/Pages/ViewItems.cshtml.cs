f41485041d17a9824e481ea713b360a1f13b21b7
Sokari <patsoks.sokari@gmail.com>
2020-04-18 17:27:45 +0100

    add view item page

---
+++ AdminApp/Pages/ViewItems.cshtml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AdminApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+
+namespace AdminApp.Pages
+{
+    public class ViewItemsModel : PageModel
+    {
+        private readonly AppDbContext _db;
+
+        public ViewItemsModel(AppDbContext db)
+        {
+            _db = db;
+        }
+
+        public IEnumerable<StoreItems> Items { get; set; }
+        public async Task OnGet()
+        {
+            Items = await _db.StoreItems.ToListAsync();
+        }
+    }
+}
